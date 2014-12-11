f17de5b2627d1205bc8b37d00f3bd78a95699742
Joe Near <jnear@csail.mit.edu>
2014-12-11 16:29:08 -0500

    more changes

--- lib/smc/exp.rb
+++ lib/smc/exp.rb
@@ -10,6 +10,29 @@ class Exp
     "Exp.new(" + ([@type] + @args).map{|x| x.to_exp}.join(", ") + ")"
   end
   alias :to_s :to_exp
+
+  def to_alloy
+    case @args.length
+    when 0
+      @type.to_s
+    when 1
+      @args[0].to_s
+    when 2
+      a, b = @args
+      a.to_alloy + "." + b.to_alloy
+    else
+      a, b, c = @args.take(3)
+      if ["+","-", "implies", "==", "and", "or"].include? b then
+        #puts @args.map{|x| x.to_s + ": " + x.class.to_s + " = " + x.to_alloy.to_s}.join(", ")
+        a.to_alloy + " " + 
+          b.to_alloy + " " + 
+          c.to_alloy
+      else
+        a.to_alloy + "." + b.to_alloy + "[" + 
+          @args.drop(3).map{|x| x.to_alloy}.join(", ") + "]"
+      end
+    end
+  end
 end
 
 class Object
@@ -22,16 +45,19 @@ class Symbol
   def to_exp
     JSON.generate(self.to_s, quirks_mode: true)
   end
+  #alias :to_alloy :to_exp
 end
 
 class String
   def to_exp
     JSON.generate(self, quirks_mode: true)
   end
+  alias :to_alloy :to_s
 end
 
 class NilClass
   def to_exp
     ":nil"
   end
+  #alias :to_alloy :to_exp
 end
--- lib/smc/main.rb
+++ lib/smc/main.rb
@@ -9,6 +9,27 @@ end
 
 require '/home/ubuntu/derailer/lib/derailer/viz/exposures.rb'
 
-puts $read_exposures
-puts $data_model
+# puts $read_exposures
+# puts $data_model
 
+def mk_data_model_sigs
+  alloy_sigs = $data_model.map{|klass, fields|
+    "sig " + klass + " extends Object {\n" +
+    fields.map{|field, type| "  " + field + ": " + type}.join(",\n") +
+    "\n}\n"}.join("\n")
+  alloy_sigs
+end
+
+def mk_one_op(exposure)
+  exposure.path.to_alloy
+end
+
+def mk_op_sigs
+  $read_exposures.map{|x| mk_one_op(x)}.join("\n")
+end
+
+
+
+puts mk_data_model_sigs
+
+puts mk_op_sigs
